42b5c887433e5356adf4a6c759e4c3cd8660c63a
Çağrı Dai <114882717+cagridai@users.noreply.github.com>
2024-01-02 17:03:35 +0300

    final commit

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -4,14 +4,23 @@ import Link from "next/link";
 import Collection from "@/components/shared/Collection";
 import { auth } from "@clerk/nextjs";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+
   const organizedEvents = await getEventsByUser({
     userId,
-    page: 1,
+    page: eventsPage,
   });
   return (
     <>
@@ -25,20 +34,19 @@ const ProfilePage = async () => {
         </div>
       </section>
 
-      {/*
-        <section className="wrapper my-8">
-          <Collection
-            data={events?.data}
-            emptyTitle="No event tickets purchased yet"
-            emptyStateSubtext="No worries, plenty of exciting events to explore!"
-            collectionType="My_Tickets"
-            limit={3}
-            page={1}
-            urlParamName="ordersPage"
-            totalPages={2}
-          />
-        </section>
-      */}
+      <section className="wrapper my-8">
+        <Collection
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries, plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
+
       {/*EVENTS ORGANIZED*/}
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -56,10 +64,10 @@ const ProfilePage = async () => {
           emptyTitle="No events have been created yet"
           emptyStateSubtext="Go create some now!"
           collectionType="Events_Organized"
-          limit={6}
-          page={1}
+          limit={3}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,11 +1,12 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import Card from "@/components/shared/Card";
+import Pagination from "@/components/shared/Pagination";
 
 type CollectionProps = {
   data: IEvent[];
   emptyTitle: string;
   emptyStateSubtext: string;
-  page: number;
+  page: number | string;
   limit: number;
   totalPages?: number;
   urlParamName?: string;
@@ -41,6 +42,14 @@ const Collection = ({
               );
             })}
           </ul>
+
+          {totalPages > 1 && (
+            <Pagination
+              urlParamName={urlParamName}
+              page={page}
+              totalPages={totalPages}
+            />
+          )}
         </div>
       ) : (
         <div className="flex-center wrapper mih-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
