eb266fdcbd27ab1fb1aca056fc07d29eadc89ba7
Çağrı Dai <114882717+cagridai@users.noreply.github.com>
2024-01-03 10:08:39 +0300

    problem with event image on safari & ios devices handled

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -24,13 +24,15 @@ const EventDetails = async ({
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-          <Image
-            src={event.imageUrl}
-            alt={"hero image"}
-            width={1000}
-            height={1000}
-            className="h-full min-h-[300px] object-cover object-center"
-          />
+          <div>
+            <Image
+              src={event.imageUrl}
+              alt={"hero image"}
+              width={1000}
+              height={1000}
+              className="h-full min-h-[300px] object-cover object-center"
+            />
+          </div>
 
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
